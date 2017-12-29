6d1f608c0c54375e65a62f94942f9a23436119d5
jmenaguale22 <jmena1@u.brockport.edu>
2017-12-29 11:01:26 -0500

    Created the app Router, I think?

--- app/hyperloop/components/app.rb
+++ app/hyperloop/components/app.rb
@@ -3,6 +3,14 @@ class App < Hyperloop::Router
   route do
     # link to React app.js for routing
     # https://github.com/gothinkster/react-redux-realworld-example-app/blob/cde269d554ac2f1f298c56dbeff1fbd12ee08b92/src/components/App.js#L59
-    Route('/', exact: true, mounts: Article)
+    Route('/', exact: true, mounts: Home)
+    Route('/login', mounts: Login)
+    Route('/register', mounts: Register)
+    Route('/editor/:slug', mounts: Editor)
+    Route('/editor', mounts: Editor)
+    Route('article/:id', mounts: Article)
+    Route('settings', mounts: Settings)
+    Route('/@:username/favorites', mounts: ProfileFavorites)
+    Route('/@:username', mounts: Profile)
   end
 end
